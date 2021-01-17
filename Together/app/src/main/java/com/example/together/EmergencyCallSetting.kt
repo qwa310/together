3fbb529778ec682597fef4d1a902dee02a0d1540
emptyshores <73519714+emptyshores@users.noreply.github.com>
2021-01-17 19:22:54 +0900

    Add files via upload

---
+++ Together/app/src/main/java/com/example/together/EmergencyCallSetting.kt
@@ -0,0 +1,21 @@
+package com.example.together
+
+import android.content.Intent
+import android.content.Intent.ACTION_PICK
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class EmergencyCallSetting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.emergency_call_setting)
+
+        findViewById<Button>(R.id.add_number).setOnClickListener {
+            val contactIntent = Intent(Intent.ACTION_PICK,ContactsContract.Contacts.CONTENT_URI)
+            startActivity(contactIntent)
+            finish()
+        }
+    }
+}

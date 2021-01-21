ba2963d559d6de913f78bf478a65875a6aa1391d
qwa310 <wjdalsdkcla@naver.com>
2021-01-22 06:33:00 +0900

    change: tmap api

--- Together/app/src/main/java/com/example/together/SurroundingsFragment.kt
+++ Together/app/src/main/java/com/example/together/SurroundingsFragment.kt
@@ -1,94 +1,48 @@
 package com.example.together
 
-import android.content.Context
 import android.os.Bundle
-import android.os.WorkSource
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.UiThread
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Transformations.map
-import com.example.together.databinding.FragmentSurroundingsBinding
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.util.FusedLocationSource
-
+import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+import com.skt.Tmap.TMapGpsManager
+import com.skt.Tmap.TMapView
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SurroundingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SurroundingsFragment : Fragment(), OnMapReadyCallback {
-    private lateinit var locationSource: FusedLocationSource  // 최적의 위치를 반환
-    private lateinit var naverMap: NaverMap
+class SurroundingsFragment : Fragment() {
+    var mapView: RelativeLayout? = null
+    var tMapView: TMapView? = null
+    var tmapgps: TMapGpsManager? = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_surroundings, container, false)
 
-    private lateinit var mContext: Context
+        mapView = view.findViewById(R.id.map)
+        tMapView = TMapView(activity)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        mContext = container!!.context
+        //Setting view of Map
+        tMapView!!.setSKTMapApiKey("l7xx52f2020a27a646b995dab1ba21acdfd7")
+        //tMapView!!.setLocationPoint(126.970325, 37.556152)
+        //tMapView!!.setCenterPoint(126.970325, 37.556152)
+        //tMapView!!.setCompassMode(false)
+        //tMapView!!.setIconVisibility(true)
+        //tMapView!!.zoomLevel = 8
+        //tmapgps!!.setMinTime(1000)
+        //tmapgps!!.setMinDistance(5F)
+        //tmapgps!!.setProvider(TMapGpsManager.GPS_PROVIDER) //gps 기반 위치 탐색
+        //tmapgps!!.setProvider(TMapGpsManager.NETWORK_PROVIDER) //네트워크 기반 위치 탐색
+        //tmapgps!!.OpenGps()
 
-        val binding = DataBindingUtil.inflate<FragmentSurroundingsBinding>(inflater,
-                R.layout.fragment_surroundings, container, false)
-
-        // 지도를 화면에 나타내는 부분
-        // 현재 fragment 안에 MapFragment를 배치해야하므로 childFragmentManager 사용
-        val fm = childFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-                ?: MapFragment.newInstance().also { location ->
-                    fm.beginTransaction().add(R.id.map, location).commit()
-                }
-
-        mapFragment.getMapAsync(this)
-
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-
-        return binding.root
-    }
-
-
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<out String>,
-                                            grantResults: IntArray) {
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            if (!locationSource.isActivated) {  // 권한 거부됨
-                naverMap.locationTrackingMode = LocationTrackingMode.None
-                Log.i("PermissionResult", "권한 거부됨")
-            }
-            return
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-
-    @UiThread
-    override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
-        naverMap.locationSource = locationSource
-
-        Log.i("onMapReady", "onMapReady 호출")
-
-        // 위치 추적 활성화, 카메라와 베어링이 사용자를 따라 움직임
-        this.naverMap.locationTrackingMode = LocationTrackingMode.Follow
-
-        // 위치 변경 이벤트, 위치 추적 테스트용
-//        this.naverMap.addOnLocationChangeListener {
-//            Toast.makeText(mContext, "${it.latitude}, ${it.longitude}",
-//                    Toast.LENGTH_SHORT).show()
-//        }
-    }
-
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+        //화면 현재 위치로 이동
+        tMapView!!.setTrackingMode(true)
+        tMapView!!.setSightVisible(true)
+        tMapView!!.mapType = TMapView.MAPTYPE_STANDARD
+        //tMapView!!.setLanguage(TMapView.LANGUAGE_KOREAN)
+        mapView!!.addView(tMapView)
+        return view
     }
 }
